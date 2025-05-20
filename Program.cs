e0377d0aaf78b5cbcc9ffcc95360e652d0257575
JadersonGasper <jadersondegasper@gmail.com>
2025-05-19 22:15:10 -0300

    estudos sobre get e set

--- Program.cs
+++ Program.cs
@@ -1,2 +1,92 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.ConstrainedExecution;
+
+//get; set;
+
+class Produto
+{
+    private string nome; // atributo (ou campo) — é um membro da classe declarado com tipo e nome, e usado		             para armazenar dados
+
+    public string Nome // propriedade com acessores (ou método: get; set). Não armazena valor, que 			           armazena valor é o atributo. A propriedade é um filtro de acesso do atributo.
+    {
+        get // retorna o valor do atributo (lê o valor do atributo) ← aqui o get faz a ligação: "valor da				 propriedade vem do atributo"
+        {
+            return nome; // o valor lido será do atributo nome.
+        }
+        set // atribui valor ao atributo (escrever no atributo)
+        {
+            if (value.Length > 1)
+                nome = value;
+            else
+                throw new Exception(“Nome do produto deve ter pelo menos 2 caracteres.”);
+        }
+    }
+}
+
+using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Classes, Objetos e Escopos de Visibilidade");
+
+        Produto p1 = new Produto();
+        p1.Nome = "Banana"; // quando você usa uma atribuição (=) em uma propriedade com set definido, o				   compilador automaticamente entende que você quer executar o set daquela propriedade.
+
+        Console.WriteLine(p1.Nome);
+    }
+}
+
+/*using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Classes, Objetos e Escopos de Visibilidade");
+
+        Produto p1 = new Produto();
+        p1.nome = "";
+        Console.WriteLine($"Seu nome é: {p1.nome} ");
+    }
+}
+
+class Produto
+{
+    public string nome; // campo
+    public string Nome //propriedade; serve como a "porta de entrada controlada" para acessar e modificar a variável nome
+    {
+        get
+        {
+            return nome; // é aqui que vincula a propriedade Nome à variável nome.
+        }
+        set
+        {
+            if (value.Length > 1)
+                nome = value;
+            else
+                throw new Exception("Nome do produto deve ter pelo menos 2 caracteres.");
+        }
+    }
+}*/
+
+// Instanciando um objeto de uma classe
+/*using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Classes, Objetos e Escopos de Visibilidade");
+
+        Produto p1 = new Produto();
+        p1.nome = "Ricardo";
+        Console.WriteLine($"Seu nome é: {p1.nome} ");
+    }
+}
+
+class Produto
+{
+    public string nome;
+}*/
+
--- obj/Debug/net8.0/testeEtestes.AssemblyInfo.cs
+++ obj/Debug/net8.0/testeEtestes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("testeEtestes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0987c0379d5083f8815c83f871e7b071cc9b0ab8")]
 [assembly: System.Reflection.AssemblyProductAttribute("testeEtestes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("testeEtestes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
